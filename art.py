d6f000d69e5984fbfaec6252b21b65526cc9338f
mukeshcs2018 <mukesh.cs2018@gmail.com>
2021-12-26 00:30:16 +0530

    Initial

---
+++ art.py
@@ -0,0 +1,24 @@
+logo  = """
+                                                                                                                          
+        CCCCCCCCCCCCC                     ffffffffffffffff      ffffffffffffffff                                          
+     CCC::::::::::::C                    f::::::::::::::::f    f::::::::::::::::f                                         
+   CC:::::::::::::::C                   f::::::::::::::::::f  f::::::::::::::::::f                                        
+  C:::::CCCCCCCC::::C                   f::::::fffffff:::::f  f::::::fffffff:::::f                                        
+ C:::::C       CCCCCC   ooooooooooo     f:::::f       ffffff  f:::::f       ffffff    eeeeeeeeeeee        eeeeeeeeeeee    
+C:::::C               oo:::::::::::oo   f:::::f               f:::::f               ee::::::::::::ee    ee::::::::::::ee  
+C:::::C              o:::::::::::::::o f:::::::ffffff        f:::::::ffffff        e::::::eeeee:::::ee e::::::eeeee:::::ee
+C:::::C              o:::::ooooo:::::o f::::::::::::f        f::::::::::::f       e::::::e     e:::::ee::::::e     e:::::e
+C:::::C              o::::o     o::::o f::::::::::::f        f::::::::::::f       e:::::::eeeee::::::ee:::::::eeeee::::::e
+C:::::C              o::::o     o::::o f:::::::ffffff        f:::::::ffffff       e:::::::::::::::::e e:::::::::::::::::e 
+C:::::C              o::::o     o::::o  f:::::f               f:::::f             e::::::eeeeeeeeeee  e::::::eeeeeeeeeee  
+ C:::::C       CCCCCCo::::o     o::::o  f:::::f               f:::::f             e:::::::e           e:::::::e           
+  C:::::CCCCCCCC::::Co:::::ooooo:::::o f:::::::f             f:::::::f            e::::::::e          e::::::::e          
+   CC:::::::::::::::Co:::::::::::::::o f:::::::f             f:::::::f             e::::::::eeeeeeee   e::::::::eeeeeeee  
+     CCC::::::::::::C oo:::::::::::oo  f:::::::f             f:::::::f              ee:::::::::::::e    ee:::::::::::::e  
+        CCCCCCCCCCCCC   ooooooooooo    fffffffff             fffffffff                eeeeeeeeeeeeee      eeeeeeeeeeeeee  
+                                                                                                                          
+                                                                                                                          
+                                                                                                                          
+                                                                                                                          
+                                                                                                                          
+                                                                                                                          """
